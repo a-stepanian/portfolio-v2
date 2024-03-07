1d9cb822eee28884fc34b2d55c0d97c94316ac20
Alex Stepanian <alexstepanian11@gmail.com>
2024-03-06 17:57:10 -0800

    Basic NextJS SPA implementation

---
+++ src/AppWrapper.tsx
@@ -0,0 +1,10 @@
+import { App } from "./App";
+import { AppContextProvider } from "./AppContext";
+
+export default function AppWrapper() {
+  return (
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+  );
+}
